685b77af846abf5d1bde0b55f7bed9598272308a
tomracanelli <willie.e.reed@proton.me>
2024-11-03 23:14:14 -0500

    init

---
+++ Readme.md
@@ -0,0 +1,7 @@
+-Install library
+
+-Run command
+npm start
+
+-version
+node version 16.13.0
---
+++ frontend/src/app/constants/config.ts
@@ -0,0 +1,19 @@
+/*
+Project : Cryptotrades
+FileName :  config.ts
+Author : LinkWell
+File Created : 21/07/2021
+CopyRights : LinkWell
+Purpose : This is the file which contain all global constants used in the application
+*/
+export const config = {
+    site_name: "Cryptotrades",
+    // media_path: "https://exampleapi.com",
+    media_path: "https://pulseart.io",
+    // base_url: "https://pulseart.io",
+    base_url: "http://localhost:4200",
+    google_client_id:'116318305608-1dgsvgdiss1fc9h8s7po0as10sjbhvnm.apps.googleusercontent.com',
+    facebook_client_id:'',
+    blockchain:"Rinkeby",
+    blockchain_url:"https://rinkeby.etherscan.io/"
+}
